980513495ffb144a845c06de0a9b882b8a3f8ded
unknown <sebastian.cardona-p@labinfo.is.escuelaing.edu.co>
2025-10-22 09:51:11 -0500

    before simulacro

---
+++ proxy/src/main/java/co/edu/escuelaing/proxy/ProxiController.java
@@ -0,0 +1,103 @@
+package co.edu.escuelaing.proxy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProxiController {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private int indexUrl = 0;
+    private static String serversURL[] = {
+        "http://localhost:8080/",
+        "http://localhost:8081/"};
+
+    @GetMapping("/catalan")
+    public String getCatalan(@RequestParam(name = "value") int value) throws IOException {
+        System.out.println("Consulta a server: " + serversURL[indexUrl]);
+        String url = serversURL[indexUrl] + "catalan?value=" + value;
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        System.out.println("Hice bien con con httpurl: ");
+        con.setRequestMethod("GET");
+        System.out.println("Hice bien get: ");
+
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        System.out.println("Hice bien userAgent: ");
+
+        int responseCode;
+
+        try {
+            responseCode = con.getResponseCode();
+        } catch (Exception e) {
+            System.out.println("Hice mal getResponseCode pues fallo este server, debo cambairlo: ");
+            System.out.println("El server " + serversURL[indexUrl] + " fallo, debo cambiar al otro server ya mismooo");
+            System.out.println("Consulta a server: " + serversURL[indexUrl]);
+            indexUrl++;
+            System.out.println("Ahora consulta a server: " + serversURL[indexUrl % serversURL.length]);
+            String url1 = serversURL[indexUrl % serversURL.length] + "catalan?value=" + value;
+            URL obj1 = new URL(url1);
+            HttpURLConnection con1 = (HttpURLConnection) obj1.openConnection();
+            con1.setRequestMethod("GET");
+            con1.setRequestProperty("User-Agent", USER_AGENT);
+
+            responseCode = con1.getResponseCode();
+        }
+        System.out.println("Hice bien getResponseCode: ");
+
+        System.out.println("GET Response Code :: " + responseCode);
+        System.out.println(responseCode == HttpURLConnection.HTTP_OK);
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new java.io.BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println("Response from server: " + response.toString());
+            return response.toString();
+        } else {
+            System.out.println("El server " + serversURL[indexUrl] + " fallo, debo cambiar al otro server ya mismooo");
+            System.out.println("Consulta a server: " + serversURL[indexUrl]);
+            indexUrl++;
+            System.out.println("Ahora consulta a server: " + serversURL[indexUrl % serversURL.length]);
+            String url1 = serversURL[indexUrl % serversURL.length] + "catalan?value=" + value;
+            URL obj1 = new URL(url1);
+            HttpURLConnection con1 = (HttpURLConnection) obj1.openConnection();
+            con1.setRequestMethod("GET");
+            con1.setRequestProperty("User-Agent", USER_AGENT);
+
+            int responseCode1 = con1.getResponseCode();
+            System.out.println("GET Response Code :: " + responseCode1);
+            if (responseCode1 == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new java.io.BufferedReader(new InputStreamReader(
+                        con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // print result
+                System.out.println("Response from server: " + response.toString());
+                return response.toString();
+            } else {
+                return "None of the servers works";
+            }
+        }
+    }
+}
